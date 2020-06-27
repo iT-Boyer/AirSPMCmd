d4a3fd8de090c7d58c76062e57ab87c96b146fce
iTBoyer <iTBoyer@qq.com>
2020-06-28 00:31:16 +0800

    功能点：使用命令行工具接收终端参数 问题：

---
+++ Sources/FFMPEG/ffmpeg2.swift
@@ -0,0 +1,59 @@
+#!/usr/bin/swift sh
+
+import SwiftFFmpeg // sunlubo/SwiftFFmpeg
+
+if CommandLine.argc < 2 {
+    print("Usage: \(CommandLine.arguments[0]) <input file>")
+    exit(1)
+}
+let input = CommandLine.arguments[1]
+
+let fmtCtx = try AVFormatContext(url: input)
+try fmtCtx.findStreamInfo()
+
+fmtCtx.dumpFormat(isOutput: false)
+
+guard let stream = fmtCtx.videoStream else {
+    fatalError("No video stream.")
+}
+guard let codec = AVCodec.findDecoderById(stream.codecParameters.codecId) else {
+    fatalError("Codec not found.")
+}
+let codecCtx = AVCodecContext(codec: codec)
+codecCtx.setParameters(stream.codecParameters)
+try codecCtx.openCodec()
+
+let pkt = AVPacket()
+let frame = AVFrame()
+
+while let _ = try? fmtCtx.readFrame(into: pkt) {
+    defer { pkt.unref() }
+
+    if pkt.streamIndex != stream.index {
+        continue
+    }
+
+    try codecCtx.sendPacket(pkt)
+
+    while true {
+        do {
+            try codecCtx.receiveFrame(frame)
+        } catch let err as AVError where err == .tryAgain || err == .eof {
+            break
+        }
+
+        let str = String(
+            format: "Frame %3d (type=%@, size=%5d bytes) pts %4lld key_frame %d",
+            codecCtx.frameNumber,
+            frame.pictureType.description,
+            frame.pktSize,
+            frame.pts,
+            frame.isKeyFrame
+        )
+        print(str)
+
+        frame.unref()
+    }
+}
+
+print("Done.")
